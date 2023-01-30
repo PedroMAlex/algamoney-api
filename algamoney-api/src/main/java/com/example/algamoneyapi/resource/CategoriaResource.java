fed90544162eb5eb60298b381ca2735004cdbbc3
PedroMAlex <pedromelo16072004@gmail.com>
2023-01-30 19:47:07 -0300

    conexão com o banco de dados

--- algamoney-api/src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.example.algamoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.Repository.CategoriaRepository;
@@ -15,11 +24,12 @@ import com.example.algamoneyapi.Repository.CategoriaRepository;
 public class CategoriaResource {
 
 	@Autowired
-	private 	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
+
 	
 }
